23e4dea259f2056e741e8b4fd51ae14b067dea21
Victor Soares <victorgamer132br@gmail.com>
2024-12-07 11:27:08 -0300

    Finalizado para testes

--- src/js/actionButton.js
+++ src/js/actionButton.js
@@ -1,41 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Ajusta para garantir que o site comece no topo ao recarregar
-    // if (window.location.hash !== '#inicio') {
-    //     window.location.hash = '#inicio';
-    // }
-
     const backToTopButton = document.getElementById('backToTop');
     const whatsappButton = document.getElementById('whatsappButton');
 
-    // Função para exibir ou esconder os botões ao rolar a página
-    function toggleButtons() {
-        const scrollPosition = window.scrollY;
-        if (scrollPosition > 100) {
-            backToTopButton.style.display = 'flex';
-            whatsappButton.style.display = 'flex';
-            setTimeout(() => {
-                backToTopButton.style.opacity = '1';
-                whatsappButton.style.opacity = '1';
-            }, 10);
-        } else {
-            backToTopButton.style.opacity = '0';
-            whatsappButton.style.opacity = '0';
-            setTimeout(() => {
-                backToTopButton.style.display = 'none';
-                whatsappButton.style.display = 'none';
-            }, 500);
-        }
+    if (!whatsappButton) {
+        console.error("Botões não encontrados. Verifique os IDs no HTML.");
+        return;
     }
 
-    // Adiciona o evento de scroll para exibir ou esconder os botões
+    function toggleButtons() {
+        const shouldShow = window.scrollY > 200;
+        whatsappButton.classList.toggle('show', shouldShow);
+    }
+
     document.addEventListener('scroll', toggleButtons);
 
-    // Ação ao clicar no botão "Voltar ao Topo"
-    backToTopButton.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
 
-    // Ação ao clicar no botão do WhatsApp
     whatsappButton.addEventListener('click', function () {
         window.open('https://wa.me/message/6G4BFG2XBSRQP1', '_blank');
     });
